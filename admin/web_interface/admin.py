cc2873d7cb39384781f4fbbc451c76c4eeec9a45
Alexei Bushuev <alexeib.2018@gmail.com>
2018-08-23 04:11:28 +0300

    managed Customer, Items, Customer_items by django admin

--- admin/web_interface/admin.py
+++ admin/web_interface/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
-from .models import Customer
+from .models import Customer, Item, CustomerItem
 
 class CustomerAdmin(admin.ModelAdmin):
 	list_display = ('name', 'email', 'password', 'active')
 
+
+class ItemAdmin(admin.ModelAdmin):
+	list_display = ('description',)
+
+
+class CustomerItemAdmin(admin.ModelAdmin):
+	list_display = ('customer', 'item')
+
+
 admin.site.register(Customer, CustomerAdmin)
+admin.site.register(Item, ItemAdmin)
+admin.site.register(CustomerItem, CustomerItemAdmin)
--- admin/web_interface/models.py
+++ admin/web_interface/models.py
@@ -1,10 +1,32 @@
 from django.db import models
 
+
 class Customer(models.Model):
 	name = models.CharField(max_length=200, default='')
 	email = models.CharField(max_length=200, default='')
 	password = models.CharField(max_length=200, default='')
 	active = models.IntegerField(default=1)
 
+	def __str__(self):
+		return '%s <%s>' % (self.name, self.email)
+
 	class Meta:
 		db_table = 'customer'
+
+
+class Item(models.Model):
+	description = models.CharField(max_length=200, default='')
+
+	def __str__(self):
+		return self.description
+
+	class Meta:
+		db_table = 'items'
+
+
+class CustomerItem(models.Model):
+	customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+	item = models.ForeignKey(Item, on_delete=models.CASCADE)
+
+	class Meta:
+		db_table = 'customer_items'
