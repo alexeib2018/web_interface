755bbe1674f25816a2e71dc787cca9ba76d82bb0
Alexei Bushuev <alexeib.2018@gmail.com>
2018-08-29 14:20:02 +0300

    separate out the DB connect info

---
+++ settings.pl
@@ -0,0 +1,9 @@
+use warnings;
+use strict;
+
+our $dbhost = "ec2-54-83-51-78.compute-1.amazonaws.com";
+our $dbname = "db9ptvci6bt8cc";
+our $username = "sjypzttvwajipn";
+our $password = "ec2c4d06d5d65d1ef826d5488c3cd885609826bdca1ee1359996ca99895288b9";
+
+1;
